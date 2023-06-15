c74b28bcc94a3a439ac6be38d8904ba5468961a1
Hem Pun <hemthegentlmen@gmail.com>
2023-06-15 17:26:35 +0545

    Car Details Model Added

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -8,17 +8,27 @@ const CustomButton = ({
   containerStyles,
   handleClick,
   btnType,
+  textStyles,
+  rightIcon,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
       type={btnType}
       className={`custom-btn ${containerStyles}`}
-      onClick={() => {
-        handleClick;
-      }}
+      onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="rightIcon"
+            fill
+            className="object-contain"
+          ></Image>
+        </div>
+      )}
     </button>
   );
 };
